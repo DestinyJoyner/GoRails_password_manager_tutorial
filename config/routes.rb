6fb7bd9971d96a7e7c1e526ea6653ad6c2990a0d
DestinyJoyner <destinyjoyner@pursuit.org>
2025-02-07 11:21:23 -0500

    shared and remove access to passwords with other users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   # shortcut for all the routes for the passwords resource
-  resources :passwords
-
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :passwords do
+  # routes for password sharing btw users add a nested route
+  resources :shares, only: [:new, :create, :destroy]
+  # command rails routes -g shares to see all nested routes
+      # password_shares GET  /passwords/:password_id/shares(.:format)   shares#index
+      # POST   /passwords/:password_id/shares(.:format)  shares#create
+      # new_password_share GET /passwords/:password_id/shares/new(.:format) shares#new
+      # edit_password_share GET    /passwords/:password_id/shares/:id/edit(.:format) shares#edit
+      # password_share GET    /passwords/:password_id/shares/:id(.:format) shares#show
+      # PATCH  /passwords/:password_id/shares/:id(.:format)  shares#update
+      # PUT    /passwords/:password_id/shares/:id(.:format)  shares#update
+      # DELETE /passwords/:password_id/shares/:id(.:format) shares#destroy
+  end
+  
 
 
   # Defines the root path route ("/")
